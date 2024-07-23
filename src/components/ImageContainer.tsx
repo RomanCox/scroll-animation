37879ee18b0fc2c4d20801e039543dae12a917ef
RomanCox <rom.cox.dev@gmail.com>
2024-07-23 13:12:41 +0300

    change console.log

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {memo, MutableRefObject, RefObject, useLayoutEffect, useRef} from "react";
+import {memo, MutableRefObject, RefObject, useRef} from "react";
 import Image from "next/image";
 import gsap from "gsap";
 import {useGSAP} from '@gsap/react';
